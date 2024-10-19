a938391a3b8561660385c379f6e7c90197a1bc08
unknown <fasil2001.pp@gmail.com>
2024-10-20 04:04:58 +0530

    first commit

---
+++ js/form_submit.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_v8cmeze","template_qkvi3y4",parms).then(alert("Email Sent.!!"))
+}
